ed10a7143ccffe4e029d9625a02ba48761cf6ac4
maks19801 <maks19801@gmail.com>
2020-10-24 22:33:43 +0300

    Added fragment_content.xml and ContentFragment.java

---
+++ app/src/main/java/com/example/weatherl2/ContentFragment.java
@@ -0,0 +1,28 @@
+package com.example.weatherl2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ContentFragment newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ContentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+        TextView textView = (TextView) view.findViewById(R.id.city);
+        return view;
+    }
+}
